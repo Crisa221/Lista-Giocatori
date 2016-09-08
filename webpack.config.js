4c0c6e941d0496729dbeceadc330afa2c6354d84
Nicolò <thenicolaus22@gmail.com>
2016-09-08 15:10:45 +0200

    Modificato webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
-var path = require('path')
-var wabpack = require('webpack')
 
-modeule.exports = {
-    entry: [
-        'babel-polyfill',
-    ],
-    output: {
-        publicPath: '/',
-        filename: 'index.js'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: path.join(__dirname, 'src'),
-                loader: 'babel-loader',
-                query: {
-                    presets: ["es2015"],
-                }
-            }
-        ]
-    },
-    debug: true
-}
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/scripts.js",
+  output: {
+    path: __dirname + "/js",
+    filename: "scripts.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+};
