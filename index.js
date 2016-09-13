49f190d927d7cf9fd846a1ae27d1232153d6ff12
Nicolò <thenicolaus22@gmail.com>
2016-09-13 15:39:45 +0200

    Modificato path reducer

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { reduPlayer, initState } from './reducer'
+import { reduPlayer, initState } from './reducer/reducer.js'
 // import App from '/component'
 
 let store = createStore(reduPlayer, initState);
