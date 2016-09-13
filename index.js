bcdee06cd5e90ed8d4c526ae2af7383c1054d9d0
Nicolò <thenicolaus22@gmail.com>
2016-09-13 17:28:50 +0200

    Modificato reducer

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { reduPlayer, initState } from './reducer/reducer.js'
+import { reducer, initState } from './reducer/reducer.js'
 import App from './component/component.js'
 
-let store = createStore(reduPlayer, initState);
+let store = createStore(reducer, initState);
 
 render(
     <Provider store={store}>
-        <App />
+        <table />
+        <form />
     </Provider>,
     document.getElementById('app')
 )
