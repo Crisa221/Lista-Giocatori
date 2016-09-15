860e77335340a5fb74b86ea732e0ab1aef3e7746
Nicolò <thenicolaus22@gmail.com>
2016-09-15 17:50:38 +0200

    Ultima commit del lavoro ora inizia quella personale ;)

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import { reducer, initState } from './reducer/reducer.js'
-import App from './component/component.js'
+import rootReducer from './reducer/reducer.js'
 
-let store = createStore(reducer, initState);
+const store = createStore( rootReducer );
 
 render(
     <Provider store={store}>
         <table />
         <form />
     </Provider>,
-    document.getElementById('app')
+    document.getElementById('root')
 )
